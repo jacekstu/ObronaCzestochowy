cc4299c6dcf802cff042dfbe0ee0a4dd0bb186e7
jacekstu <studzinski.jacek@o2.pl>
2020-12-02 19:26:26 +0000

    Removed some postion_x logic from the villain.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from bullet import Bullet
 from player import Player
 from villain import Villain
 
+
+# All postion available for villains on the x axis
+position_x_lt = [0,150,350, 450, 750]
+
 # Initialize the library
 pygame.init()
 pygame.mouse.set_visible(False)
@@ -47,17 +51,24 @@ while True:
 	timer -= dt
 	if timer <= 0:
 		if len(villain_group) <= 5:
-			print(len(villain_group))
+			# select the position of the villan on the x - axis
+			# is the position x free - 
+			key_dt = position_x_lt[random.randint(0,4)]
 			idx = random.randint(0,6)
-			villain = Villain(idx)
+			villain = Villain(idx, key_dt)
 			villain_group.add(villain)
 		timer = 3
+
+
+############## chujowe rzwiazanie ZROB TAK, ZE ODEJMUJESZ TE WARTOSCI Z LISTY, A POTEM JE Z POWROTEM DODAJESZ, INACZEJ EDZEI CHUJNIA Z GRZYBNIA!
+
+
 
 	for bullet in bullet_group:
 		gets_hit = pygame.sprite.spritecollide(bullet, villain_group, True)
 		if gets_hit:
+			# change the value for this pos-x key to True so it can be selected again
 			bullet_group.remove(bullet)
-
 
 
 	screen.fill(BLACK)
--- villain.py
+++ villain.py
@@ -1,17 +1,15 @@
 import pygame, random
 from settings import *
 
-pos_lt_x = [0,150,350,450,750]
 pos_lt_y = [0,150,250,350,450]
 
 class Villain(pygame.sprite.Sprite):
-	def __init__(self, idx):
+	def __init__(self, idx, x_pos):
 		super().__init__()
 		self.image = VILLAIN_LT[idx]
-		self.image = pygame.transform.scale(self.image, (100,100))
-		self.pos_x = pos_lt_x[random.randint(0, len(pos_lt_x) - 1)]
-		pos_y = pos_lt_y[random.randint(0,3)]
-		self.rect = self.image.get_rect(center = (self.pos_x, pos_y))
+		self.pos_x = x_pos
+		self.pos_y = pos_lt_y[random.randint(0,3)]
+		self.rect = self.image.get_rect(center = (self.pos_x, self.pos_y))
 		self.speed = -1
 		self.max_up = self.rect.y + 50
 		self.max_down = self.rect.y - 50
